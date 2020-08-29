a26d3f23c50c48463974415731fb13a1bc78c7c4
Ashwin A Nayar <ashwin5059198@gmail.com>
2020-08-29 20:04:31 +0530

    Cleanup, defined globals and functions

--- ws_build.sh
+++ ws_build.sh
@@ -1,31 +1,65 @@
 #!/usr/bin/env bash
-# build project
+
+#########################################################################################
+# GLOBALS
+#########################################################################################
+
+# Configuration file for Doxygen
+DOXYGEN_CONFIG_FILE="mosaic.dconfig"
+
+# All these directories will be deleted permanently when clean in invoked
+DIRS_TO_TRASH=(
+    bin
+    build
+    docs
+)
+
+#########################################################################################
+# FUNCTIONS
+#########################################################################################
 
 function ws_build () {
+    
     cmake -H. -Bbuild && \
     cmake --build build -- -j8 && \
     echo -e "\n\033[1;33m Workspace build complete \033[0m\n"
+    
 }
 
 function gendoc () {
-    if [[ -f mosaic.dconfig ]]
+    
+    if [[ -f $DOXYGEN_CONFIG_FILE ]]
     then
-        doxygen mosaic.dconfig && \
+        
+        doxygen $DOXYGEN_CONFIG_FILE && \
         cd docs/latex
         make && \
         echo -e "\n\033[1;33m Generated docs \033[0m\n"
+        
     else
+        
         echo -e "\n\033[1;33m config file not found! Skipping docs auto-generation \033[0m\n"
+        
     fi
 }
 
 function cleanup () {
-    if [[ -d bin ]]; then rm -rf bin; fi
-    if [[ -d build ]]; then rm -rf build; fi
-    if [[ -d docs ]]; then rm -rf docs; fi
+    
+    for dir in "${DIRS_TO_TRASH[@]}"; do
+        if [[ -d $dir ]]
+        then
+            
+            rm -rf $dir
+            
+        fi
+    done
     
     echo -e "\n\033[1;33m Workspace cleaned \033[0m\n"
 }
+
+#########################################################################################
+# ENTRY POINT
+#########################################################################################
 
 if [[ $1 = "gendoc" ]]
 then

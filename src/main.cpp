5ceb0b3fa3fa0ee33f1f04b0e9ed0b7019f81070
Ashwin A Nayar <ashwin5059198@gmail.com>
2020-09-01 23:09:27 +0530

    Tested TCP connection with module

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,45 @@
 #include <ros/package.h>
+#include <ros/ros.h>
 
 #include <mosaic_gnss_driver/mosaic_gnss.h>
 
-int main(int argc, char **argv)
+void connectViaPcap(void)
 {
+    mosaic_gnss_driver::MosaicGNSS gnss;
+
     std::string thisPackagePath = ros::package::getPath("mosaic_gnss_driver");
     std::string testFile = thisPackagePath + "/test/data/mosaic_capture_001.pcap";
-
-    mosaic_gnss_driver::MosaicGNSS gnss;
 
     gnss.connect(testFile, mosaic_gnss_driver::MosaicGNSS::PCAP);
 
     gnss.processData();
 
     gnss.disconnect();
+
+}
+
+void connectViaTcp(void)
+{
+    mosaic_gnss_driver::MosaicGNSS gnss;
+
+    std::string device = "192.168.3.1:80";
+
+    gnss.connect(device, mosaic_gnss_driver::MosaicGNSS::TCP);
+
+    gnss.processData();
+
+    gnss.disconnect();
+
+}
+
+int main(int argc, char **argv)
+{
+
+    ros::init(argc, argv, "sample_node", ros::init_options::AnonymousName);
+    ros::NodeHandle nh;
+
+    // connectViaPcap();
+    connectViaTcp();
 
     return 0;
 }

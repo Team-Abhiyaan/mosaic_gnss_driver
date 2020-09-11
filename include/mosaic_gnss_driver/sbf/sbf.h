67e649b5f50084632b9c07ba71eec8ed680ffc7f
Suraj Rathi <suraj.rathi00@gmail.com>
2020-09-11 17:00:35 +0530

    convert `data_start + data_length` to `data_end`

--- include/mosaic_gnss_driver/sbf/sbf.h
+++ include/mosaic_gnss_driver/sbf/sbf.h
@@ -5,20 +5,23 @@
 
 namespace sbf {
     class SBF {
+        // Start of Block String
         static constexpr const uint8_t sync_chars[2] = {'$', '@'};
 
-        // Data Storages
+        // Internal Buffer Bounds and Usage
         static const size_t buffer_size = 256; // Approximately max message size
         char _buffer[buffer_size] = {0}; // Any point in std::array ?
         uint8_t *const buffer = reinterpret_cast<uint8_t *const>(_buffer);
         size_t buffer_use{0};
 
+        // Data Bounds
         const uint8_t *data_start{nullptr};
-        size_t data_length{0};
+        const uint8_t *data_end{nullptr};
 
-        //
-        const uint8_t *read_ptr{nullptr};
-        const uint8_t *block_start{nullptr};
+
+        const uint8_t *read_ptr{nullptr}; // Current position in buffer + data
+        const uint8_t *block_start{nullptr}; // Indicates the start of found block.
+
 
         // Helper functions for read()
         bool in_buffer(const uint8_t *const ptr) {
@@ -26,12 +29,13 @@ namespace sbf {
         }
 
         bool in_data(const uint8_t *const ptr) {
-            return data_start <= ptr && ptr < data_start + data_length;
+            return data_start <= ptr && ptr < data_end;
         }
 
-        bool seek_block();
 
         const uint8_t *read(size_t size);
+
+        bool seek_block();
 
         bool parse_block();
 

1236d429cf1da56faf46ad4149a033260ae37d56
Suraj Rathi <suraj.rathi00@gmail.com>
2020-09-17 15:06:29 +0530

    ros version should work

--- src/main.cpp
+++ src/main.cpp
@@ -2,53 +2,50 @@
 #include <ros/ros.h>
 
 #include <mosaic_gnss_driver/mosaic_gnss.h>
+#include <mosaic_gnss_driver/connections/pcap.h>
 
 void connectViaPcap()
 {
-    mosaic_gnss_driver::MosaicGNSS gnss;
-
     std::string thisPackagePath = ros::package::getPath("mosaic_gnss_driver");
     std::string testFile = thisPackagePath + "/test/data/capture_002.pcap";
 
-    gnss.connect(testFile, mosaic_gnss_driver::MosaicGNSS::PCAP);
+    mosaic_gnss_driver::GNSS<mosaic_gnss_driver::connections::PCAP, sbf::SBF> gnss{};
+    if (!gnss.conn.connect(testFile)) return;
+    while (gnss.tick());
 
-    while (gnss.isConnected() && gnss.processData() == mosaic_gnss_driver::MosaicGNSS::READ_SUCCESS);
-
-    gnss.bufferDump();
-
-    gnss.disconnect();
+    // gnss.disconnect(); // Destructors automatically disconnect
 
 }
 
-void connectViaTcp()
-{
-    mosaic_gnss_driver::MosaicGNSS gnss;
-
-    std::string device = "192.168.3.1:3001";
-
-    gnss.connect(device, mosaic_gnss_driver::MosaicGNSS::TCP);
-
-	for (int i = 0; i < 50; i++)
-	{
-		bool success = gnss.isConnected() && gnss.processData() == mosaic_gnss_driver::MosaicGNSS::READ_SUCCESS;
-
-		if (!success)
-        {
-			std::cout << "Error, non successful termination" << std::endl;
-           	std::cout << gnss.errorMsg() << std::endl;
-
-			break;
-		}
-        else
-        {
-			std::cout << "Got stream: " << i + 1 << std::endl;
-		}
-	}
-
-	gnss.bufferDump();
-    gnss.disconnect();
-
-}
+// void connectViaTcp()
+// {
+//     mosaic_gnss_driver::MosaicGNSS gnss;
+//
+//     std::string device = "192.168.3.1:3001";
+//
+//     gnss.connect(device, mosaic_gnss_driver::MosaicGNSS::TCP);
+//
+// 	for (int i = 0; i < 50; i++)
+// 	{
+// 		bool success = gnss.isConnected() && gnss.processData() == mosaic_gnss_driver::MosaicGNSS::READ_SUCCESS;
+//
+// 		if (!success)
+//         {
+// 			std::cout << "Error, non successful termination" << std::endl;
+//            	std::cout << gnss.errorMsg() << std::endl;
+//
+// 			break;
+// 		}
+//         else
+//         {
+// 			std::cout << "Got stream: " << i + 1 << std::endl;
+// 		}
+// 	}
+//
+// 	gnss.bufferDump();
+//     gnss.disconnect();
+//
+// }
 
 int main(int argc, char **argv)
 {
@@ -56,8 +53,8 @@ int main(int argc, char **argv)
     ros::init(argc, argv, "sample_node", ros::init_options::AnonymousName);
     ros::NodeHandle nh;
 
-    // connectViaPcap();
-    connectViaTcp();
+    connectViaPcap();
+    // connectViaTcp();
 
     return 0;
 }

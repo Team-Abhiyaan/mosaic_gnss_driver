b7419f0868977046f74d6dfe1d3a764e7ad8fe5b
sachidanandvs <65409657+sachidanandvs@users.noreply.github.com>
2020-10-14 21:09:25 +0530

    added nmea parser now pub navsatfix

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,39 @@
 #include <ros/package.h>
 #include <ros/ros.h>
+#include <sensor_msgs/NavSatFix.h>
 
+#include <mosaic_gnss_driver/parsers/nmeaparse/NMEAParser.h>
+#include <mosaic_gnss_driver/parsers/nmeaparse/GPSService.h>
 #include <mosaic_gnss_driver/mosaic_gnss.h>
 
 #include <mosaic_gnss_driver/parsers/sbf/sbf.h>
 #include <mosaic_gnss_driver/connections/pcap.h>
 
-void connectViaPcap()
+void connectViaPcap(const std::string &data_type)
 {
     std::string thisPackagePath = ros::package::getPath("mosaic_gnss_driver");
-    std::string testFile = thisPackagePath + "/test/data/capture_002.pcap";
+    if(data_type == "sbf"){
+        std::string testFile = thisPackagePath + "/test/data/capture_002.pcap";
+        mosaic_gnss_driver::GNSS<mosaic_gnss_driver::connections::PCAP, sbf::SBF> gnss{};
+        if (!gnss.connect(testFile)) return;
+        while (gnss.tick());
+    }
+    if(data_type == "nmea"){
+        std::string testFile = thisPackagePath + "/test/data/nmea/capture_004.pcap";
+        mosaic_gnss_driver::GNSS<mosaic_gnss_driver::connections::PCAP, nmea::NMEAParser> gnss{};
+        if (!gnss.connect(testFile)) return;
+        ros::NodeHandle n;
+        ros::Publisher pub = n.advertise<sensor_msgs::NavSatFix>("NavSatFix", 10);
+        sensor_msgs::NavSatFix navSatFix;
+        while (gnss.nmea_tick(navSatFix)) {
+        
+            pub.publish(navSatFix);
 
-    mosaic_gnss_driver::GNSS<mosaic_gnss_driver::connections::PCAP, sbf::SBF> gnss{};
-    if (!gnss.conn.connect(testFile)) return;
-    while (gnss.tick());
+            ros::spinOnce();
+        };
+    }
+
+    
 
     // gnss.disconnect(); // Destructors automatically disconnect
 
@@ -54,8 +74,10 @@ int main(int argc, char **argv)
 
     ros::init(argc, argv, "sample_node", ros::init_options::AnonymousName);
     ros::NodeHandle nh;
+    std::string data_type;
+    nh.param<std::string>("data_type",data_type,"nmea");
 
-    connectViaPcap();
+    connectViaPcap(data_type);
     // connectViaTcp();
 
     return 0;

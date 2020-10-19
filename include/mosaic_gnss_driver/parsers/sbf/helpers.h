e830c8f17cec84b926105ee8970b5857b29e02e2
Suraj Rathi <suraj.rathi00@gmail.com>
2020-10-19 12:15:50 +0530

    Properly rename all types

--- include/mosaic_gnss_driver/parsers/sbf/helpers.h
+++ include/mosaic_gnss_driver/parsers/sbf/helpers.h
@@ -24,80 +24,51 @@ namespace sbf
     static_assert(sizeof(float) == 4, "Bad float size, no 4 byte floating point type");
     static_assert(sizeof(double) == 8, "Bad double size, no 8 byte floating point type");
 
-    using float4_t = float;
-    using float8_t = double;
+    using float32_t = float;
+    using float64_t = double;
+
+    using c1 = char;
+    using u1 = uint8_t;
+    using u2 = uint16_t;
+    using u4 = uint32_t;
+    using f4 = float32_t;
+    using f8 = float64_t;
 
 #pragma pack(push, 1) // Packs the struct tightly, no gaps b/w objs
     struct Header
     { /// SBF Header
         char sync_chars[2];
-        uint16_t CRC;
-        uint16_t ID;
-        uint16_t length;
+        u2 CRC;
+        u2 ID;
+        u2 length;
     };
     struct PVTGeodetic
     { /// SBF BlockNum 4007
-        uint32_t TOW;
-        uint16_t WNc;
-        uint8_t Mode, Error;
-        float8_t Latitude, Longitude, Height;
-        float4_t undulation, vn, ve, vu, cog;
-        float8_t rxclkbias;
-        float4_t rxclkdrifk;
-        uint8_t time_system, datum, num_satellites, wa_corr_info;
-        uint16_t referenceID, mean_corr_age;
-        uint32_t signal_info;
-        uint8_t alert_flag, num_bases;
-        uint16_t ppp_info, latency, haccuracy;
-        uint8_t misc;
+        u4 TOW;
+        u2 WNc;
+        u1 Mode, Error;
+        f8 Latitude, Longitude, Height;
+        f4 undulation, vn, ve, vu, cog;
+        f8 rxclkbias;
+        f4 rxclkdrifk;
+        u1 time_system, datum, num_satellites, wa_corr_info;
+        u2 referenceID, mean_corr_age;
+        u4 signal_info;
+        u1 alert_flag, num_bases;
+        u2 ppp_info, latency, haccuracy;
+        u1 misc;
     };
+
+    struct PosCovGeodetic
+    { /// SBF BlockNum 4007
+        u4 TOW;
+        u2 WNc;
+        u1 Mode, Error;
+        // lat - Latitude, lon - Longitude, hgt - Height, bias - Clock Bias
+        f4 lat_lat, lon_lon, hgt_hgt, bias_bias, lat_lon, lat_hgt, lat_bias, lon_hgt, lon_bias, hgt_bias;
+    };
+
 #pragma pack(pop)
-
-    /// Helper Functions for converting sbf types to c types
-    uint8_t u1(const uint8_t *buffer)
-    {
-        return *buffer;
-    }
-
-    uint16_t u2(const uint8_t *buffer)
-    {
-        return *reinterpret_cast<const uint16_t *>(buffer);
-    }
-
-    uint32_t u4(const uint8_t *buffer)
-    {
-        return *reinterpret_cast<const uint32_t *>(buffer);
-    }
-
-    int8_t i1(const uint8_t *buffer)
-    {
-        return *reinterpret_cast<const int8_t *>(buffer);
-    }
-
-    int16_t i2(const uint8_t *buffer)
-    {
-        return *reinterpret_cast<const int16_t *>(buffer);
-    }
-
-    int32_t i4(const uint8_t *buffer)
-    {
-        return *reinterpret_cast<const int32_t *>(buffer);
-    }
-
-    float4_t f4(const uint8_t *buffer)
-    {
-        return *reinterpret_cast<const float4_t *>(buffer);
-    }
-
-    float8_t f8(const uint8_t *buffer)
-    {
-        return *reinterpret_cast<const float8_t *>(buffer);
-    }
-
-    std::string c(const uint8_t *buffer, size_t size)
-    {
-        return std::string{reinterpret_cast<const char *>(buffer), size};
-    }
 
     /// Gets the ID and Revision Number from the SBF ID field
     std::pair<uint16_t, uint8_t> parse_id(const uint16_t raw_id)

a411eef998b550048c86ed5edf559fe83dd09c41
Ashwin A Nayar <ashwin5059198@gmail.com>
2021-01-29 15:06:41 +0530

    add node as a class

---
+++ include/mosaic_gnss_driver/node.hpp
@@ -0,0 +1,128 @@
+#ifndef __MOSAIC_GNSS_DRIVER_NODE_H__
+#define __MOSAIC_GNSS_DRIVER_NODE_H__
+
+#include <ros/ros.h>
+
+#include <mosaic_gnss_driver/mosaic_gnss.h>
+#include <mosaic_gnss_driver/data_buffers.h>
+
+#include <mosaic_gnss_driver/connections/pcap.h>
+#include <mosaic_gnss_driver/connections/tcp.h>
+#include <mosaic_gnss_driver/connections/udp.h>
+#include <mosaic_gnss_driver/connections/serial.h>
+
+#include <mosaic_gnss_driver/parsers/nmeaparse/NMEAParser.h>
+#include <mosaic_gnss_driver/parsers/sbf/sbf.h>
+
+template <typename c_Tp, typename p_Tp>
+class MosaicGNSSDriverNode
+{
+public:
+    MosaicGNSSDriverNode(ros::NodeHandle &nh, ros::NodeHandle &pnh)
+        : m_nh(nh), m_pnh(pnh),
+          m_sbf_pvt_type("geodetic"),
+          m_publish_navsatfix(false),
+          m_publish_velocity(false),
+          m_publish_pose(false),
+          m_publish_nmea_sentence(false),
+          m_publish_time_reference(false)
+    {
+        _initParams();
+        _initPublishers();
+    }
+
+    bool registerDevice(const std::string &device)
+    {
+        return m_gnss.connect(device);
+    }
+
+    void spin()
+    {
+        auto start_time = ros::Time::now();
+        const auto publish_duration = ros::Duration(0.2);
+        while (ros::ok() && m_gnss.tick())
+        {
+
+#ifdef MOSAIC_GNSS_FAKE_SLEEP_TIME
+            ros::Duration(MOSAIC_GNSS_FAKE_SLEEP_TIME).sleep();
+#endif
+
+            const auto cur = ros::Time::now();
+            if (cur - start_time > publish_duration)
+            { // We should publish
+                start_time = cur;
+
+                // Publish fields
+                m_dBuff.publish_all();
+            }
+
+            ros::spinOnce();
+        }
+    }
+
+private:
+    void _initParams()
+    {
+        m_pnh.getParam("sbf_pvt_type", m_sbf_pvt_type);
+        m_pnh.getParam("publish/navsatfix", m_publish_navsatfix);
+        m_pnh.getParam("publish/velocity", m_publish_velocity);
+        m_pnh.getParam("publish/pose", m_publish_pose);
+        m_pnh.getParam("publish/nmea_sentence", m_publish_nmea_sentence);
+        m_pnh.getParam("publish/time_reference", m_publish_time_reference);
+    }
+
+    void _initPublishers()
+    {
+        bool isGeodetic = false;
+
+        if (m_sbf_pvt_type == "cartesian")
+        {
+            isGeodetic = false;
+            m_gnss.p.parsers.enable_cartesian();
+        }
+        else
+        {
+            isGeodetic = true;
+            if (m_sbf_pvt_type != "geodetic")
+                ROS_WARN("Invalid pvt type, assuming geodetic.");
+
+            m_gnss.p.parsers.enable_geodetic();
+        }
+
+        if (m_publish_navsatfix)
+        {
+            m_dBuff.nav_sat_fix.init(m_nh, "navsatfix", 5, false);
+        }
+        if (m_publish_velocity)
+        {
+            m_dBuff.velocity.init(m_nh, "velocity", 5, false);
+        }
+        if (m_publish_pose)
+        {
+            m_dBuff.pose.init(m_nh, "pose", 5, false);
+        }
+        if (m_publish_nmea_sentence)
+        {
+            m_dBuff.nmea_sentence.init(m_nh, "nmea_sentence", 5, false);
+        }
+        if (m_publish_time_reference)
+        {
+            m_dBuff.time_reference.init(m_nh, "time_reference", 5, false);
+        }
+    }
+
+    ros::NodeHandle m_nh, m_pnh;
+
+    mosaic_gnss_driver::DataBuffers m_dBuff;
+
+    mosaic_gnss_driver::GNSS<c_Tp, p_Tp> m_gnss{m_dBuff};
+
+    std::string m_sbf_pvt_type;
+    bool m_publish_navsatfix;
+    bool m_publish_velocity;
+    bool m_publish_pose;
+    bool m_publish_nmea_sentence;
+    bool m_publish_time_reference;
+};
+
+#endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,217 +1,87 @@
-#include <ros/ros.h>
-#include <ros/package.h>
-#include <ros/console.h>
-#include <mosaic_gnss_driver/data_buffers.h>
-
-#include <mosaic_gnss_driver/mosaic_gnss.h>
-
-#include <mosaic_gnss_driver/connections/pcap.h>
-#include <mosaic_gnss_driver/connections/tcp.h>
-#include <mosaic_gnss_driver/connections/udp.h>
-#include <mosaic_gnss_driver/connections/serial.h>
-
-#include <mosaic_gnss_driver/parsers/nmeaparse/NMEAParser.h>
-#include <mosaic_gnss_driver/parsers/sbf/sbf.h>
-
+#include <mosaic_gnss_driver/node.hpp>
 #include <boost/filesystem.hpp>
-
-namespace fs = boost::filesystem;
+#include <ros/package.h>
 
 template <typename c_Tp, typename p_Tp>
-struct driverImpl
+void launchDriver(const std::string &device)
 {
-    static void run(const std::string &device) {}
-};
+    ros::NodeHandle nh, pnh("~");
 
-template <typename conn_type, typename parser_type>
-void driver(const std::string &device)
-{
-    driverImpl<conn_type, parser_type>::run(device);
+    MosaicGNSSDriverNode<c_Tp, p_Tp> node(nh, pnh);
+    if (!node.registerDevice(device))
+    {
+        ROS_FATAL("Unable to register device");
+        exit(EXIT_FAILURE);
+    }
+
+    node.spin();
 }
 
-template <typename conn_type>
-struct driverImpl<conn_type, sbf::SBF>
+template <typename p_Tp>
+void initDriver(std::string &device, const std::string &conn)
 {
-    static void run(const std::string &device)
+    if (conn == "pcap")
     {
-        ros::NodeHandle nh, pnh("~");
-        mosaic_gnss_driver::DataBuffers buf;
-
-        mosaic_gnss_driver::GNSS<conn_type, sbf::SBF> gnss{buf};
-        if (!gnss.connect(device))
-            return;
-
-        bool geodetic;
-        const auto type = pnh.param("sbf_pvt_type", std::string{"geodetic"});
-
-        if (type == "cartesian")
-        {
-            geodetic = false;
-        }
-        else
-        {
-            geodetic = true;
-            if (type != "geodetic")
-                ROS_WARN("Invalid pvt type, assuming geodetic.");
-        }
-
-        buf.velocity.init(nh, "velocity", 5, false);
-
-        if (geodetic)
-        {
-            buf.nav_sat_fix.init(nh, "nav_sat_fix", 5, false);
-            gnss.p.parsers.enable_geodetic();
-        }
-        else
-        {
-            buf.pose.init(nh, "pose", 5, false);
-            gnss.p.parsers.enable_cartesian();
-        }
-
-        auto start_time = ros::Time::now();
-
-        const auto publish_duration = ros::Duration(0.2);
-
-        while (ros::ok() && gnss.tick())
-        {
-
-#ifdef MOSAIC_GNSS_FAKE_SLEEP_TIME
-            ros::Duration(MOSAIC_GNSS_FAKE_SLEEP_TIME).sleep();
-#endif
-
-            const auto cur = ros::Time::now();
-            if (cur - start_time > publish_duration)
-            { // We should publish
-                start_time = cur;
-
-                // Publish fields
-                buf.publish_all();
-            }
-
-            ros::spinOnce();
-        }
-    }
-};
-
-template <typename conn_type>
-struct driverImpl<conn_type, nmea::NMEAParser>
-{
-    static void run(const std::string &device)
-    {
-        ros::NodeHandle nh, pnh("~");
-        mosaic_gnss_driver::DataBuffers buf;
-
-        mosaic_gnss_driver::GNSS<conn_type, nmea::NMEAParser> gnss{buf};
-        if (!gnss.connect(device))
-            return;
-
-        buf.nav_sat_fix.init(nh, "nav_sat_fix", 5, false);
-        buf.pose.init(nh, "pose", 5, false);
-        buf.velocity.init(nh, "velocity", 5, false);
-        buf.time_reference.init(nh, "time_reference", 5, false);
-
-        bool pub_nmea_msg;
-        ros::param::get("/mosaic_gnss/pub_nmea_msg", pub_nmea_msg);
-        if (pub_nmea_msg)
-        {
-            buf.nmea_sentence.init(nh, "nmea_sentence", 5, false);
-        }
-
-        auto start_time = ros::Time::now();
-
-        const auto publish_duration = ros::Duration(0.2);
-
-        while (ros::ok() && gnss.tick())
-        {
-
-#ifdef MOSAIC_GNSS_FAKE_SLEEP_TIME
-            ros::Duration(MOSAIC_GNSS_FAKE_SLEEP_TIME).sleep();
-#endif
-
-            const auto cur = ros::Time::now();
-            if (cur - start_time > publish_duration)
-            { // We should publish
-                start_time = cur;
-
-                // Publish fields
-                buf.publish_all();
-            }
-
-            ros::spinOnce();
-        }
-    }
-};
-
-template <typename parser_type>
-void start(std::string &device, const std::string &type)
-{
-    if (type == "pcap")
-    {
+        namespace fs = boost::filesystem;
         // safely join paths
         fs::path dir(ros::package::getPath("mosaic_gnss_driver"));
         fs::path file(device);
         fs::path full_path = dir / file;
         device = full_path.string();
 
-        driver<mosaic_gnss_driver::connections::PCAP, parser_type>(device);
+        launchDriver<mosaic_gnss_driver::connections::PCAP, p_Tp>(device);
     }
-    else if (type == "serial")
+    else if (conn == "serial")
     {
-        driver<mosaic_gnss_driver::connections::Serial, parser_type>(device);
+        launchDriver<mosaic_gnss_driver::connections::Serial, p_Tp>(device);
     }
-    else if (type == "tcp")
+    else if (conn == "tcp")
     {
-        driver<mosaic_gnss_driver::connections::TCP, parser_type>(device);
+        launchDriver<mosaic_gnss_driver::connections::TCP, p_Tp>(device);
     }
-    else if (type == "udp")
+    else if (conn == "udp")
     {
-        driver<mosaic_gnss_driver::connections::UDP, parser_type>(device);
+        launchDriver<mosaic_gnss_driver::connections::UDP, p_Tp>(device);
     }
 }
 
 int main(int argc, char **argv)
 {
+    ros::init(argc, argv, "mosaic_gnss");
+    ros::NodeHandle pnh("~");
 
-    ros::init(argc, argv, "mosaic_gnss"); //, ros::init_options::AnonymousName);
-    ros::NodeHandle nh, pnh("~");
-
-    std::string device, parser, type, gps_link;
-    bool pub_nmea_msg;
+    bool deviceRegistered = false;
+    std::string device, stream, connection;
 
     if (!pnh.getParam("device", device))
     {
         ROS_FATAL("No device specified");
         return EXIT_FAILURE;
     }
-    if (!pnh.getParam("stream", parser))
+    if (!pnh.getParam("stream", stream))
     {
         ROS_FATAL("Stream type not specified");
         return EXIT_FAILURE;
     }
-    if (!pnh.getParam("connection", type))
+    if (!pnh.getParam("connection", connection))
     {
         ROS_FATAL("Connection type not specified");
         return EXIT_FAILURE;
     }
 
-    if (!pnh.getParam("pub_nmea_msg", pub_nmea_msg))
-        pnh.setParam("pub_nmea_msg", 0);
-
-    if (!pnh.hasParam("frame_id"))
-        pnh.setParam("frame_id", "gps_link");
-
-    if (parser == "sbf")
+    if (stream == "sbf")
     {
-        start<sbf::SBF>(device, type);
+        initDriver<sbf::SBF>(device, connection);
     }
-    else if (parser == "nmea")
+    else if (stream == "nmea")
     {
-        start<nmea::NMEAParser>(device, type);
+        initDriver<nmea::NMEAParser>(device, connection);
     }
     else
     {
         ROS_FATAL("Invalid parser type set.");
+        return EXIT_FAILURE;
     }
 
-    return 0;
+    return EXIT_SUCCESS;
 }

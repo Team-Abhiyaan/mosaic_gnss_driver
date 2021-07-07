534edafdb300f2f532365147a59c3884857feb0f
Dhruv Maroo <dhruvmaru007@gmail.com>
2021-07-07 14:01:08 +0530

    Removed inline from the `parse_id` function in `helpers.h` (#16)
    Move definition of `parse_id` to `sbf.cpp`
    
    * Removed inline from the parse_id function in helpers.h
    
    helpers.h contained the parse_id function's definition, rather than the declaration, and hence, when it was being included at multiple places, it led to failure of building the program. Now the definition (placed in a new `helpers.cpp` file) has been separated from the declaration, so it builds successfully without the inline qualifier.
    
    * Placed the function definition in sbf.cpp

--- src/core/parsers/sbf/sbf.cpp
+++ src/core/parsers/sbf/sbf.cpp
@@ -49,6 +49,11 @@ void sbf::SBF::parse(const uint8_t* const data, size_t size)
         ;
 }
 
+std::pair<uint16_t, uint8_t> sbf::parse_id(const uint16_t raw_id)
+{
+    return {raw_id & 0b0001111111111111u, (raw_id & 0b1110000000000000u) >> 13u};
+}
+
 bool sbf::SBF::parse_block()
 {
     const uint8_t* ret;
